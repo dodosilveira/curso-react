434156b1731e1af498d972497aa34958e8b0c70e
dodosilveira <douglas.silveira@processmind.com.br>
2022-05-25 19:49:01 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-sequences */
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App ( props ) {
+
+  const mudarNome = (event) => {
+    console.log(event.target.value)
+  }
+
+  const criaComboBox = () => {
+    const opcoes = [ "Fulano", "Ciclano" ]
+    const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  return(
+    <>
+      {criaComboBox()}
+      <h1>Hello {props.nome}</h1>
+      <input type="text" value={props.nome} onChange={mudarNome} />
+    </>
+  )
 }
 
 export default App;
